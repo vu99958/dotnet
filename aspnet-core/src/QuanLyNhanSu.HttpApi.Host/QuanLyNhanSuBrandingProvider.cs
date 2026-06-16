a8112585f13e1284902a31ab615f53a39b1bd622
Tran Thanh Vu <vu99958@gmail.com>
2026-06-16 23:37:45 +0700

    Khoi tao du an ABP QuanLyNhanSu va cau hinh hoan thien SQL Server

---
+++ aspnet-core/src/QuanLyNhanSu.HttpApi.Host/QuanLyNhanSuBrandingProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+using QuanLyNhanSu.Localization;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace QuanLyNhanSu;
+
+[Dependency(ReplaceServices = true)]
+public class QuanLyNhanSuBrandingProvider : DefaultBrandingProvider
+{
+    private IStringLocalizer<QuanLyNhanSuResource> _localizer;
+
+    public QuanLyNhanSuBrandingProvider(IStringLocalizer<QuanLyNhanSuResource> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public override string AppName => _localizer["AppName"];
+}
